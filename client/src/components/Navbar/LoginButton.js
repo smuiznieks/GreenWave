22c5d302640bc3ac7160598cd8c00d7182a104d2
Selga Muiznieks <selga.muiznieks@gmail.com>
2018-03-28 21:34:06 -0400

    create user page and clean up comments

--- client/src/components/Navbar/LoginButton.js
+++ client/src/components/Navbar/LoginButton.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import FlatButton from 'material-ui/FlatButton';
 
 const LoginButton = (props) => (
   <button className="btn" onClick={props.onClick}>Log In</button>
--- client/src/components/Navbar/LoginMenu.js
+++ client/src/components/Navbar/LoginMenu.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import IconButton from 'material-ui/IconButton';
+// import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+// import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 
 const LoginMenu = (props) => {
   const { onLogOut, username, ...otherProps } = props;
