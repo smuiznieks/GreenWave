8570a62e3311fb5cbd3c1c1eeaf7ad64cdee0218
Selga Muiznieks <selga.muiznieks@gmail.com>
2018-03-29 20:48:09 -0400

    landing page edits

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -5,47 +5,16 @@ import { List, ListItem } from '../../components/List';
 import { withUser } from '../../services/withUser';
 
 class HomePage extends Component {
-  state = {
-    stuff: null
-  }
-  componentDidMount() {
-    // only try loading stuff if the user is logged in.
-    if (!this.props.user) {
-      return;
-    }
-
-    axios.get('/api/stuff')
-      .then(res => {
-        this.setState({
-          stuff: res.data
-        });
-      })
-      .catch(err => {
-        // if we got an error, we'll just log it and set stuff to an empty array
-        console.log(err);
-        this.setState({
-          stuff: []
-        });
-      });
-  }
-  
   render() {
-    const { user } = this.props; // get the user prop from props
-    const { stuff } = this.state; // get stuff from state
-
+    // get the user prop from props
+    const { user } = this.props; 
     return (
       <Container>
         <Row>
-          {user && stuff &&
+          {user &&
             <div>
               Welcome back, {user.username}!
-              <List>
-                {stuff.map((s, i) => <ListItem key={i} primaryText={s} />)}
-              </List>
             </div>
-          }
-          {user && !stuff &&
-            <div>Hold on, looking for your stuff...</div>
           }
           {!user &&
             <div>Hey! I don't recognize you! Register and log in using the link above</div>
@@ -56,6 +25,4 @@ class HomePage extends Component {
   }
 }
 
-// withUser function will wrap the specified component in another component that will
-// inject the currently logged in user as a prop called "user"
 export default withUser(HomePage);
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -19,16 +19,16 @@ router.post('/auth',passport.authenticate('local'), userController.userLogin);
 
 // this route is just returns an array of strings if the user is logged in
 // to demonstrate that we can ensure a user must be logged in to use a route
-router.route('/stuff')
-  .get(mustBeLoggedIn(), (req, res) => {
-    // at this point we can assume the user is logged in. if not, the mustBeLoggedIn middleware would have caught it
-    res.json([
-      'Bears',
-      'Beets',
-      'Battlestar Galactica'
-    ]);
-  }
-);
+// router.route('/stuff')
+//   .get(mustBeLoggedIn(), (req, res) => {
+//     // at this point we can assume the user is logged in. if not, the mustBeLoggedIn middleware would have caught it
+//     res.json([
+//       'Bears',
+//       'Beets',
+//       'Battlestar Galactica'
+//     ]);
+//   }
+// );
 
 module.exports = router;
 
