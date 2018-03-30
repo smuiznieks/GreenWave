d00ed3401f08b2700a8497a572c6a01c36e0d07c
unknown <paul_schaller@att.net>
2018-03-29 21:21:10 -0400

    adding pictures and html

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Col, Row, Container } from '../../components/Grid';
+import "./HomePage.css";
 
 class HomePage extends Component {
     state = {
@@ -8,10 +9,17 @@ class HomePage extends Component {
 
     render() {
         return (
-            <Container>
+            <Container fluid>
                 <Row>
+                    <img src={require("./images/GreenWaveLogo.jpg")} alt="GreenWaveLogo" className="img-responsive" />
                     <h1>Welcome to GreenWave</h1>
                 </Row>
+                <Container classname="HomePage">
+                <h2>GreenWave is a community of environmentally<br/>
+                    conscious individuals looking to promote<br/>
+                    more sustainable living in their local communities
+                </h2>
+                </Container>
             </Container>
         );
     }
