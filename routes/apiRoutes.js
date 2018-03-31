3be7fb01ab883b99e5fe4648d6354d6fb1189082
Ashlee Brolly <ajbrolly@gmail.com>
2018-03-31 12:15:15 -0400

    upating maps

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -19,21 +19,9 @@ router.post('/auth',passport.authenticate('local'), userController.userLogin);
 // User logout
 router.delete('/auth', userController.userLogout);
 
-// this route is just returns an array of strings if the user is logged in
-// to demonstrate that we can ensure a user must be logged in to use a route
-// router.route('/stuff')
-//   .get(mustBeLoggedIn(), (req, res) => {
-//     // at this point we can assume the user is logged in. if not, the mustBeLoggedIn middleware would have caught it
-//     res.json([
-//       'Bears',
-//       'Beets',
-//       'Battlestar Galactica'
-//     ]);
-//   }
-// );
 
 // Create new location
-router.post('/locations', locationsController.saveLocation);
+router.post('/locations', locationsController.createLocation);
 
 // Create new event
 router.post('/events', eventsController.saveEvent);
