7946193ed0c6a6520ea96d9f41c1099c48a61521
unknown <paul_schaller@att.net>
2018-03-31 11:11:30 -0400

    trying to get to render.  adding corrections.

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,29 +1,30 @@
 import axios from 'axios';
 import React, { Component } from 'react';
 import { Col, Row, Container } from '../../components/Grid';
+import { withUser } from '../../services/withUser';
 import "./HomePage.css";
 
 class HomePage extends Component {
-
     state = {
         user: ''
     };
 
-    render() {
-        return (
-            <Container fluid>
-                <div className="logo">
-                    <img src={require("./images/GreenWaveLogo.jpg")} alt="GreenWaveLogo" className="img-responsive" />
-                    <h1>Welcome to GreenWave</h1>
-                </div>
-                <div className="HomePage">
-                <h2>GreenWave is a community of environmentally<br/>
-                    conscious individuals looking to promote<br/>
-                    more sustainable living in their local communities
-                </h2>
-                </div>
-            </Container>
-        );
-    }
+  render() {
+    // get the user prop from props
+    const { user } = this.props; 
+    return (
+      <Container>
+        <Row>
+          {user &&
+            <div>Hi there, {user.username}!</div>
+          }
+          {!user &&
+            <div>Hey! I don't recognize you! Log in using the link above</div>
+          }
+        </Row>
+      </Container>
+    );
+  }
+}
 
 export default withUser(HomePage);
