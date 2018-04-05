6855d86c186383fa29ab469f9a2bc753455841de
Paul Schaller <paul_schaller@att.net>
2018-04-05 18:32:11 -0400

    Update HomePage.js

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -10,17 +10,7 @@ class HomePage extends Component {
     const { user } = this.props; 
     return (
       <Container>
-        <Row>
-          <img src={require("./images/GreenWaveLogo.jpg")} alt="GreenWaveLogo" className="img-responsive" />
-          <h1>Welcome to GreenWave</h1>
-          <h2>GreenWave is a community of environmentally<br/>
-              conscious individuals looking to promote<br/>
-              more sustainable living in their local communities
-          </h2>
-          {!user &&
-            <h3>Hey! I don't recognize you! Log in using the link above</h3>
-          }
-        </Row>
+       
       </Container>
     );
   }
