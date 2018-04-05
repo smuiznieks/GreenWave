b6285355ab0778c04f5923ca76132769e7d1266b
Paul Schaller <paul_schaller@att.net>
2018-04-05 18:40:58 -0400

    Update HomePage.js

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -4,16 +4,5 @@ import { Col, Row, Container } from '../../components/Grid';
 import { withUser } from '../../services/withUser';
 import "./HomePage.css";
 
-class HomePage extends Component {
-  render() {
-    // get the user prop from props
-    const { user } = this.props; 
-    return (
-      <Container>
-       
-      </Container>
-    );
-  }
-}
 
 export default withUser(HomePage);
