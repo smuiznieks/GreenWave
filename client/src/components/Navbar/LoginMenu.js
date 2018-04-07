5c0418f3351764933ac56020ede2e0e26c391012
unknown <mkap25@yahoo.com>
2018-04-07 11:45:26 -0400

    added CSS page to navbar made it bigger for links

--- client/src/components/Navbar/LoginMenu.js
+++ client/src/components/Navbar/LoginMenu.js
@@ -3,6 +3,7 @@ import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
 // import MenuItem from 'material-ui/MenuItem';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import "./Navbar.css"
 
 const LoginMenu = (props) => {
   const { onLogOut, username, ...otherProps } = props;
