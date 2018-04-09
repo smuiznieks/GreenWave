8bad9c1bc2b4b83ea1d662fce21572f711666e92
unknown <mkap25@yahoo.com>
2018-04-09 04:40:49 -0400

    added new footer component, will work on it more

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -26,11 +26,14 @@ export const Navigation = (props) => {
     };
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+         return (
+         <nav className="navbar navbar-expand-lg navbar-light bg-light">
+         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+             </button>
+                 <span className="navbar-toggler-icon"></span>
+             </button>
+             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 {!user ? 
                     <div className="navbar-nav">
                         <LoginButton onClick={handleLogIn} />
