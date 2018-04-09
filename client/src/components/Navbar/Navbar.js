fc268f96515884ed7c69efad961482b6ec0f81d6
unknown <mkap25@yahoo.com>
2018-04-09 04:44:20 -0400

    added footer component, work in progress

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -27,7 +27,6 @@ export const Navigation = (props) => {
 
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="/">GreenWave</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
